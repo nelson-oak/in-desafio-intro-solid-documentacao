9463544eaf7e1a737e9e2037feebbfda63abc713
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-03-21 10:12:52 -0300

    Listar todos os usuarios (com validaçao de admin)

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,12 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.headers.user_id as string;
+    console.log(user_id);
+
+    const users = this.listAllUsersUseCase.execute({ user_id });
+
+    return response.json(users);
   }
 }
 
